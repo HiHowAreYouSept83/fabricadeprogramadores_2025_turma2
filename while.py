eff4953e98232840e1bcd05d77a1f8ae5f9fc36f
Matheus Martins <matheusscarpel@gmail.com>
2025-08-04 11:44:37 -0300

    Add files via upload

---
+++ while.py
@@ -0,0 +1,69 @@
+"""x=1
+while x<=3:
+    print(x)
+    x = x + 1"""
+
+"""x=1
+while x<=50000:
+    print(x)
+    x = x + 1"""
+
+"""x=1
+while x<=100:
+    print(x)
+    x = x + 1"""
+
+"""x=50
+while x<=100:
+    print(x)
+    x = x + 1"""
+
+
+"""def exercicio_3():
+    x = 10
+    while x >= 0: 
+        print(x)
+        x = x - 1
+    print('fogo') 
+exercicio_3()"""
+
+"""
+fim=int(input("digite o ultimo numero a imprimir:"))
+x=0
+while x <= fim:
+    if x % 2 == 0:
+        print(x) 
+    x = x + 1"""
+
+"""def numero_par():
+    fim=int(input("digite o ultimo numero a imprimir:"))
+    x=0
+    while x <= fim:
+        if x % 2 == 0:
+            print(x) 
+        x = x + 1
+numero_par()"""
+
+"""def numero_impar():
+    fim=int(input("digite o ultimo numero a imprimir:"))
+    x=0
+    while x <= fim:
+        if x % 2 == 1:
+            print(x) 
+        x = x + 1
+numero_impar()"""
+
+def arvore_de_natal():
+    linha = 1
+    while linha <= 10:
+        coluna = 1
+        while coluna <= linha:
+            coluna = coluna + 1
+            if coluna % 2 == 1: 
+                print('❤️', end="")
+            else: 
+                print('💚', end="")
+        print()
+        linha = linha + 1
+arvore_de_natal()
+            
