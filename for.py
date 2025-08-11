78926ed79e1314db7e5b5a6ebb3b2fff55f7a5a0
Matheus Martins <matheusscarpel@gmail.com>
2025-08-11 11:15:48 -0300

    atividades python

---
+++ for.py
@@ -0,0 +1,48 @@
+"""L=[8,9,15]
+for math in L:
+    print(math)"""
+
+"""L=["jagstang", "mustang", "jaguar", "stratocaster", "telecaster", "hollow", "jazzmaster", "les-Paul", "SG", "superstrato"]
+for s in L:
+    for letra in s:
+        print(letra)"""
+
+"""def exibe_maior_numero():
+    L=[1,7,2,4]
+    maximo=L[0]
+    for e in L:
+        if e > maximo:
+          maximo = e 
+    print(maximo)
+exibe_maior_numero()"""
+
+"""def exibe_menor_numero():
+    L=[1,7,2,4]
+    maximo=L[0]
+    for e in L:
+        if e < maximo:
+          maximo = e 
+    print(maximo)
+exibe_menor_numero()"""
+
+"""def range_exemplo():
+    for v in range(10):
+        print(v)
+range_exemplo()"""
+
+
+"""def range_inicio_e_fim():
+    for v in range(5, 8):
+        print(v)
+range_inicio_e_fim"""
+
+"""def salto():
+    for t in range(0,222,2):
+        print(t, end=" ")
+salto()"""
+
+"""nome = "matheus"
+idade = 16
+grana = 58.12
+print("%s tem %d anos e R$%5.2f no bolso." % (nome, idade, grana))"""
+
---
+++ try_except.py
@@ -0,0 +1,33 @@
+"""try:
+    divisão = 10 / 0
+    print(divisão)
+except:
+    print('''não foi possivel realizar essa operação''')"""
+
+"""def divide(x,y):
+    try:
+        result= x/y
+    except ZeroDivisionError:
+        print("por favor, não utilize zeros!")
+    except:
+        print("\033[91m algo deu errado...")
+    else:
+        print(f"seu resultado é: {result}")
+    finally:
+        print("\033[92m vamos de novo?")
+divide(13,0)"""
+
+"""def match_case():
+    import random 
+    value = random.randint(0, 2)
+
+    match value:
+        case 0:
+            print("zero!")
+        case 1:
+            print("um!")
+        case 2:
+            print("dois!")
+        case _:
+            print("exceção")
+match_case()"""
